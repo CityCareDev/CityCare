39e0c77215671746c91d94b7b9e583441b87ce1d
tamizh2225 <tamizhselvan1379@gmail.com>
2026-03-27 17:01:30 +0530

    feat:updated AdmitPatientRequest

--- src/main/java/com/citycare/dto/request/AdmitPatientRequest.java
+++ src/main/java/com/citycare/dto/request/AdmitPatientRequest.java
@@ -1,20 +1,18 @@
 package com.citycare.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
-/**
- * AdmitPatientRequest – Body for POST /patients/admit
- * Admin picks the emergencyId from GET /emergencies/dispatched
- * and provides it here to admit the citizen as a patient.
- * The system will automatically release the ambulance back to AVAILABLE.
- */
 @Data
 public class AdmitPatientRequest {
 
     @NotNull(message = "Emergency ID is required")
+    @Positive(message = "Patient ID must be a valid positive number")
+    @Min(value = 1, message = "emergency id  should be greater than 0")
     private Long emergencyId;
-
     private String ward;  // e.g. "ICU", "General Ward", "Cardiology"
     private String notes; // Admission notes
 }
+

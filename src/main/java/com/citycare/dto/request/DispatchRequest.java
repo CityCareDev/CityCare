cb6d8358b885bb6c50b83cad59db151d863e3e24
H <2478344@cognizant.com>
2026-03-30 16:36:21 +0530

    feat:update DispatchRequest.java

--- src/main/java/com/citycare/dto/request/DispatchRequest.java
+++ src/main/java/com/citycare/dto/request/DispatchRequest.java
@@ -8,9 +8,16 @@ import lombok.Data;
  * Dispatcher selects an available ambulance ID from
  * GET /emergencies/ambulances/available and provides it here.
  */
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
 @Data
 public class DispatchRequest {
 
     @NotNull(message = "Ambulance ID is required")
+    @Positive(message = "ambulance Id must be positive")
+    @Min(value = 1,message = "ambulanceId must start from 1")
     private Long ambulanceId;
 }

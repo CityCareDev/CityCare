98775bd4d63faec682dda347e9c8042223ac1370
DeepsTrisha <i.trishadeva@gmail.com>
2026-03-31 11:11:56 +0530

    feat: updated FacilityRequest

--- src/main/java/com/citycare/dto/request/FacilityRequest.java
+++ src/main/java/com/citycare/dto/request/FacilityRequest.java
@@ -1,22 +1,32 @@
 package com.citycare.dto.request;
 
+
 import com.citycare.entity.Facility;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+
+//we use dto classes, it will tell us, which data we want to show to the user or which  to hide
 
 @Data
 public class FacilityRequest {
-    @NotBlank(message = "Name is required")
+
+    @NotBlank(message = "Facility name is required")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
     private String name;
 
-    @NotNull(message = "Type is required")
+    @NotNull(message = "Facility type is required (e.g., HOSPITAL, PARK, SCHOOL)")
     private Facility.Type type;
 
-    @NotBlank(message = "Location is required")
+    @NotBlank(message = "Location address is required")
     private String location;
 
+    @Min(value = 0, message = "Capacity cannot be negative")
     private int capacity;
 
+    @NotNull(message = "Initial status is required")
     private Facility.Status status;
 }
